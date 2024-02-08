32a527f1a677c49fbee079773e5101c06cd27c33
Nassim <nassim.bensalah@esprit.tn>
2024-02-08 11:21:19 +0100

    ecmascript ex 2 + 3 | ex 4 wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
+import ClassComponent from './Components/ClassComponent'
+import FunctionalComponent from './Components/FunctionalComponent'
+import { useState } from 'react';
+
+
 
 function App() {
-  const [count, setCount] = useState(1)
-  const person = {
-    name: "name",
-    email: "test"
+  const [h, setHidden] = useState(true);
+  const tab = [
+    { name: "TWIN3", module: "REACT" },
+    { name: "TWIN4", module: "NODE" }
+  ]
+  const hello = (name) => {
+    return <p>Hello World {name}</p>
   }
-  console.log(person);
-  const fn = ()=>console.log("fn test")
-  fn()
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + "A")}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button>Ecmascript</button>
+      <p style={{ color: "red" }}>Style test</p>
+      <ul>
+        {tab.map((e, i) => <li key={i}>{e.name} & {e.module}</li>)}
+      </ul>
+      <h1>{hello("TWIN3")}</h1>
+      <button onClick={()=>setHidden(!h)}>show</button>
+      <span hidden={h}>
+      <ClassComponent name="Mondher" />
+      <FunctionalComponent name="Nassim" />
+      </span>
+      <nav
+        className="nav justify-content-center  "
+      >
+        <ul>
+          <li>
+            <Link to={'/ecmascript'}>Ecmascript</Link></li>
+          <li>
+            <Link to={'/ClassComponent'}>ClassComponent</Link></li>
+          <li>
+            <Link to={'/FunctionalComponent'}>FunctionalComponent</Link></li>
+        </ul>
+      </nav>
+
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ClassComponent from './Components/ClassComponent.jsx'
+import FunctionalComponent from './Components/FunctionalComponent.jsx'
+import Ecmascript from './Ecmascript/Ecmascript.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/", Component: App, children: [
+      { path: "/ecmascript", Component: Ecmascript},
+      { path: "/ClassComponent", Component:ClassComponent},
+      { path: "/FunctionalComponent", Component:FunctionalComponent}
+    ]
+  },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
