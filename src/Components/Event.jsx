15a66812d3eeff50e255cb79cf2b34c06246b497
Nassim <nassim.bensalah@esprit.tn>
2024-02-15 12:13:32 +0100

    atelier 2 complete

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -5,13 +5,23 @@ import { useState } from 'react';
 export default function Event(props) {
     const [e, setEvent] = useState(props.e);
     const bookEvent = () => {
-        if (e.nbTickets > 0) {
+        setEvent((e) => {
+            if (e.nbTickets > 0) {
+                var newProp = { ...e };
+                newProp.nbTickets--;
+                newProp.nbParticipants++;
+                return newProp;
+            }
+            props.showAlert();
+        });
+    };
+    const likeEvent = () => {
+        setEvent((e) => {
             var newProp = { ...e };
-            newProp.nbTickets--;
-            newProp.nbParticipants++;
-            setEvent(newProp);
-        }
-    }
+            newProp.like = !e.like;
+            return newProp;
+        });
+    };
     return <>
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={e.nbTickets == 0 ? `images/sold_out.png` : `images/${e.img}`} />
@@ -20,7 +30,8 @@ export default function Event(props) {
                 <Card.Text>Price: {e.price}</Card.Text>
                 <Card.Text>Number of tickets: {e.nbTickets}</Card.Text>
                 <Card.Text>Number of participants: {e.nbParticipants}</Card.Text>
-                <Button variant="primary" disabled={e.nbTickets==0} onClick={()=>bookEvent()}>Book an event</Button>
+                <Button variant="primary" disabled={e.nbTickets == 0} onClick={bookEvent}>Book an event</Button>
+                <Button style={{marginLeft:"5px"}} variant={e.like ? "danger" : "success"} onClick={likeEvent}>{e.like ? "Dislike" : "Like"}</Button>
             </Card.Body>
         </Card>
     </>
