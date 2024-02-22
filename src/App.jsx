907c5a9b0315d10a48117ee26e2bfea2864f541d
Nassim <nassim.bensalah@esprit.tn>
2024-02-22 11:20:29 +0100

    atelier routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { Link, Outlet } from 'react-router-dom'
-import './App.css'
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const NavBar = React.lazy(() => import("./Components/NavBar"));
+  const Ecmascript = React.lazy(() => import("./Ecmascript/Ecmascript"));
+  const Events = React.lazy(() => import("./Components/Events"));
+  const FunctionalComponent = React.lazy(() =>
+    import("./Components/FunctionalComponent")
+  );
+  const ClassComponent = React.lazy(() => import("./Components/ClassComponent"));
+  const NotFound = React.lazy(() => import("./Components/NotFound"));
+  const Home = React.lazy(() => import("./Components/Home"));
+  const LoadingComponent = () => {
+    return (
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <p>Loading...</p>
+      </div>
+    );
+  };
   return (
     <>
-      <Nav variant="tabs" defaultActiveKey="/home">
-        <Nav.Item>
-          <Nav.Link > <Link to={'/'}>Home</Link></Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link ><Link to={'/ecmascript'}>Ecmascript</Link></Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link ><Link to={'/ClassComponent'}>ClassComponent</Link></Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link ><Link to={'/FunctionalComponent'}>FunctionalComponent</Link></Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link ><Link to={'/Events'}>Events list</Link></Nav.Link>
-        </Nav.Item>
-      </Nav>
-
-      <Outlet />
+      <Suspense fallback={LoadingComponent()}>
+        <Routes>
+          <Route path="/" Component={NavBar}>
+            <Route path="/" Component={Home} />
+            <Route path="/ecmascript" Component={Ecmascript} />
+            <Route path="/events" Component={Events} />
+            <Route
+              path="/functionalComponent/:id/:number"
+              Component={FunctionalComponent}
+            />
+            <Route path="/classComponent" Component={ClassComponent} />
+          </Route>
+          <Route path="*" Component={NotFound} />
+        </Routes>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,25 @@ import ClassComponent from './Components/ClassComponent.jsx'
 import FunctionalComponent from './Components/FunctionalComponent.jsx'
 import Ecmascript from './Ecmascript/Ecmascript.jsx'
 import Events from './Components/Events.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: "/", Component: App, children: [
-      { path: "/ecmascript", Component: Ecmascript},
-      { path: "/ClassComponent", Component:ClassComponent},
-      { path: "/FunctionalComponent", Component:FunctionalComponent},
-      { path: "/Events", Component: Events},
-    ]
-  },
+// const router = createBrowserRouter([
+//   {
+//     path: "/", Component: App, children: [
+//       { path: "/ecmascript", Component: Ecmascript},
+//       { path: "/ClassComponent", Component:ClassComponent},
+//       { path: "/FunctionalComponent", Component:FunctionalComponent},
+//       { path: "/Events", Component: Events},
+//     ]
+//   },
 
-])
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
